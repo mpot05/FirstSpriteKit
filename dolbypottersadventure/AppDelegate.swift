6f6da28aedb2acf866a2508c475301821859aa45
Michael Potter <mpotter0905@gmail.com>
2024-02-26 08:00:15 -0500

    Initial Commit

---
+++ dolbypottersadventure/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  dolbypottersadventure
+//
+//  Created by Michael Potter on 2/26/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
