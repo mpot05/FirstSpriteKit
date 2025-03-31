85ac7b2d0cb636a9fd73738b6f410adad46cd9fe
Michael Potter <mpotter0905@gmail.com>
2025-03-31 10:27:44 -0400

    changed how jumps work, got rid of a ton of comments, made movement code simpler

--- dolbypottersadventure/GameScene.swift
+++ dolbypottersadventure/GameScene.swift
@@ -22,7 +22,7 @@ class GameScene: SKScene {
     
     var wall2 = SKSpriteNode()
     
-    var jumps = 1
+    var jumps = 10
     
     var level = 0
     
@@ -31,9 +31,6 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        
-        //        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
-        //        self.physicsBody = border
         wall = childNode(withName: "wall") as! SKSpriteNode
         wall.color = .brown
         
@@ -75,51 +72,38 @@ class GameScene: SKScene {
         
     }
     
-    
-    
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 123:
-            mike.physicsBody?.velocity = CGVector(dx: -275, dy: mike.physicsBody?.velocity.dy ?? 0)
-            isRight = false
-            //            mike.physicsBody?.applyImpulse(CGVector(dx: -75, dy: 0))
-            //            mike.run(SKAction.move(by: CGVector(dx: -25, dy: 0), duration: 0.2))
-        case 124:
-            mike.physicsBody?.velocity = CGVector(dx: 275, dy: mike.physicsBody?.velocity.dy ?? 0)
-            isRight = true
-            //            mike.physicsBody?.applyImpulse(CGVector(dx: 75, dy: 0))
-            //            mike.run(SKAction.move(by: CGVector(dx: 25, dy: 0), duration: 0.2))
-        case 0x31:
-            if (jumps == 1  && mike.position.y < 51){
-                mike.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 1005))
-                //                mike.run(SKAction.move(by: CGVector(dx: 0, dy: 405), duration: 0.2))
-                jumps = 0
-            }
-        case 0:
-            mike.physicsBody?.velocity = CGVector(dx: -275, dy: mike.physicsBody?.velocity.dy ?? 0)
-            isRight = false
-            //            mike.physicsBody?.applyImpulse(CGVector(dx: -75, dy: 0))
-            //            mike.run(SKAction.move(by: CGVector(dx: -25, dy: 0), duration: 0.2))
-        case 1:
-            addProjectile(mike.position,isRight)
-        case 2:
-            mike.physicsBody?.velocity = CGVector(dx: 275, dy: mike.physicsBody?.velocity.dy ?? 0)
-            isRight = true
-            //            mike.physicsBody?.applyImpulse(CGVector(dx: 75, dy: 0))
-            //            mike.run(SKAction.move(by: CGVector(dx: 25, dy: 0), duration: 0.2))
-            //            if let label = self.label {
-            //                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            //            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+            case 0: // move left with A
+                fallthrough
+            case 123: // move left with arrow key
+                mike.physicsBody?.velocity = CGVector(dx: -275, dy: mike.physicsBody?.velocity.dy ?? 0)
+                isRight = false
+            case 2: // move right with D
+                fallthrough
+            case 124: // move right with arrow key
+                mike.physicsBody?.velocity = CGVector(dx: 275, dy: mike.physicsBody?.velocity.dy ?? 0)
+                isRight = true
+            case 0x31: // jump with space
+                if (jumps == 10  /*&& mike.position.y < 51*/){
+                    mike.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 1005))
+                    jumps = 0
+                }
+            case 1: // shoot projectile with S
+                addProjectile(mike.position, isRight)
+            default:
+                print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        if mike.position.y <= 51 {
+        if jumps == 0 {
             jumps = 1
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.jumps = 10
+            }
         }
         if mike.position.x > 1024 {
             mike.position = CGPoint(x: 1, y: mike.position.y)
@@ -133,42 +117,32 @@ class GameScene: SKScene {
         }
         
         switch level {
-        case 0:
-            wall.position = CGPoint(x: -50.33248901367188,  y: 384.0007019042969)
-            austin.isHidden = true
-            austin.position = CGPoint(x: -200, y: 200)
-        case 1:
-            wall.position = CGPoint(x: 1000, y: 200)
-            wall2.position = CGPoint(x: -400, y: 0)
-            austin.isHidden = false
-            austin.position = CGPoint(x: frame.width*0.8, y: frame.height*0.1)
-            austin.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        case 2:
-            wall.position = CGPoint(x: -0.33248901367188,  y: 200)
-            austin.position = CGPoint(x: frame.width*0.2, y: frame.height*0.1)
-            wall2.position = CGPoint(x: 400, y: 300)
-            wall2.size = CGSize(width: 132.666, height: 400)
-            wall2.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 132.666, height: 400))
-            
-        default:
-            print(level)
-            level = 2
+            case 0:
+                wall.position = CGPoint(x: -50.33248901367188,  y: 384.0007019042969)
+                austin.isHidden = true
+                austin.position = CGPoint(x: -200, y: 200)
+            case 1:
+                wall.position = CGPoint(x: 1000, y: 200)
+                wall2.position = CGPoint(x: -400, y: 0)
+                austin.isHidden = false
+                austin.position = CGPoint(x: frame.width*0.8, y: frame.height*0.1)
+                austin.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            case 2:
+                wall.position = CGPoint(x: -0.33248901367188,  y: 200)
+                austin.position = CGPoint(x: frame.width*0.2, y: frame.height*0.1)
+                wall2.position = CGPoint(x: 400, y: 300)
+                wall2.size = CGSize(width: 132.666, height: 400)
+                wall2.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 132.666, height: 400))
+            default:
+                level = 2
         }
-        //        print("\(austin.isHidden)")
-        //        print("\(austin.position.x) \(austin.position.y)")
-        print(level)
-        print(wall.size)
-        //        print("y: \(mike.position.y)")
-        //        print("x: \(mike.position.x)")
-        
     }
     
     func addProjectile(_ location: CGPoint, _ facingRight: Bool)  {
-        var ball = SKSpriteNode(color: .red, size: CGSize(width: 25, height: 25))
+        let ball = SKSpriteNode(color: .red, size: CGSize(width: 25, height: 25))
         ball.position = location
         
         addChild(ball)
-        
         
         ball.physicsBody = SKPhysicsBody(rectangleOf: ball.size)
         ball.physicsBody?.friction = 0.2
@@ -176,6 +150,7 @@ class GameScene: SKScene {
         ball.physicsBody?.angularDamping = 0.1
         ball.physicsBody?.linearDamping = 0.1
         ball.physicsBody?.restitution = 0.2
+        
         if facingRight {
             ball.position.x += 30
             ball.physicsBody?.applyImpulse(CGVector(dx: 500, dy: 0))
@@ -184,8 +159,10 @@ class GameScene: SKScene {
             ball.position.x -= 30
             ball.physicsBody?.applyImpulse(CGVector(dx: -500, dy: 0))
         }
+        
         ball.physicsBody?.affectedByGravity = false
         ball.physicsBody?.allowsRotation = false
+        
         DispatchQueue.global(qos: .background).async {
             usleep(400000)
             DispatchQueue.main.async{
@@ -193,6 +170,4 @@ class GameScene: SKScene {
             }
         }
     }
-    
-    
 }
